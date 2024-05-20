4c65c96b916907af34872f5e089c352afe787442
Thomas Lacombe <tomtom637@gmail.com>
2024-05-20 09:15:32 +0200

    added drizzle orm

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+import env from "@/env";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/db/schema/index.ts",
+  out: "./src/db/migrations",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  verbose: true,
+  strict: true,
+});
